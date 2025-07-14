6fb52579b43af781073a023948e43f015815cf66
FrancescoSls97 <frankds1997@gmail.com>
2025-07-14 17:00:22 +0200

    full header navbar list

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,23 +2,41 @@ export default function Header () {
     return (
     <header>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container">
+            <div className="container p-2rem">
                 <a className="navbar-brand" href="#">
-                    <img src="../../public/img/dc-logo.png" alt="DC_logo" width="250" height="250"/>
+                    <img className="logo" src="../../public/img/dc-logo.png" alt="DC_logo" width="100" height="100"/>
                 </a>
             <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link " href="#">CHARACTERS</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Features</a>
+                            <a className="nav-link active" href="#">COMICS</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Pricing</a>
+                            <a className="nav-link" href="#">MOVIES</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                            <a className="nav-link" href="#">TV</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">GAMES</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">COLLECTIBLES</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">VIDEOS</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">FANS</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">NEWS</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">SHOP</a>
                         </li>
                     </ul>
                  </div>
